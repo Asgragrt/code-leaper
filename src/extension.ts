8542e0fc29026ce67119a91a2fdf224b9363dbd8
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2024-06-30 16:56:08 -0600

    🎨 Remove unnecessary intermediary variables

--- src/extension.ts
+++ src/extension.ts
@@ -14,37 +14,37 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    const disposable = vscode.commands.registerCommand(
-        'code-leaper.jumpNextStatement',
-        async () => {
-            const editor = vscode.window.activeTextEditor;
-            if (!editor) return;
+    context.subscriptions.push(
+        vscode.commands.registerCommand(
+            'code-leaper.jumpNextStatement',
+            async () => {
+                const editor = vscode.window.activeTextEditor;
+                if (!editor) return;
 
-            const statement = await new SelectionHelper(editor).nextStatement(
-                editor.selection.active
-            );
+                const statement = await new SelectionHelper(
+                    editor
+                ).nextStatement(editor.selection.active);
 
-            moveCursor(editor, toPosition(statement.endPosition));
-        }
+                moveCursor(editor, toPosition(statement.endPosition));
+            }
+        )
     );
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(
+        vscode.commands.registerCommand(
+            'code-leaper.jumpPrevStatement',
+            async () => {
+                const editor = vscode.window.activeTextEditor;
+                if (!editor) return;
 
-    const disposable2 = vscode.commands.registerCommand(
-        'code-leaper.jumpPrevStatement',
-        async () => {
-            const editor = vscode.window.activeTextEditor;
-            if (!editor) return;
+                const statement = await new SelectionHelper(
+                    editor
+                ).prevStatement(editor.selection.active);
 
-            const statement = await new SelectionHelper(editor).prevStatement(
-                editor.selection.active
-            );
-
-            moveCursor(editor, toPosition(statement.startPosition));
-        }
+                moveCursor(editor, toPosition(statement.startPosition));
+            }
+        )
     );
-
-    context.subscriptions.push(disposable2);
 }
 
 // This method is called when your extension is deactivated
