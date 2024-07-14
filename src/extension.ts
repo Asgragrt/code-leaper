4e3ecb47f23c5827ebc9b832b97edc2928ac3eda
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2024-07-14 17:06:04 -0600

    ✨ Reintroduce the missing commands

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,53 @@ import * as vscode from 'vscode';
 import SelectionHelper from './selectionHelper';
 import { moveCursor } from './utils';
 
+function nextStatement(editor: vscode.TextEditor) {
+    const helper = new SelectionHelper(editor);
+
+    const basePosition = editor.selection.end;
+
+    // Go to next non-empty line (ignoring comments)
+    const position = helper.isLineEmpty(basePosition)
+        ? helper.nextStart(basePosition)
+        : basePosition;
+
+    let range = helper.getCurrentStatement(position);
+
+    // Clamp to ignore characters such as \r\n
+    // Go to next statement if at the end of statement
+    if (position.isEqual(helper.clampToLine(range.end))) {
+        range = helper.getCurrentStatement(helper.nextStart(position));
+    }
+
+    // Improve selection by getting the largest at the end (should help overall)
+    range = helper.getCurrentStatement(range.end);
+
+    return range;
+}
+function prevStatement(editor: vscode.TextEditor) {
+    const helper = new SelectionHelper(editor);
+
+    const basePosition = editor.selection.start;
+
+    // Go to next non-empty line (ignoring comments)
+    const position = helper.isLineEmpty(basePosition)
+        ? helper.prevEnd(basePosition)
+        : basePosition;
+
+    let range = helper.getCurrentStatement(position);
+
+    // Clamp to ignore characters such as \r\n
+    // Go to next statement if at the end of statement
+    if (position.isEqual(helper.clampToLine(range.start))) {
+        range = helper.getCurrentStatement(helper.prevEnd(position));
+    }
+
+    // Improve selection by getting the largest at the end (should help overall)
+    range = helper.getCurrentStatement(range.end);
+
+    return range;
+}
+
 export async function activate(context: vscode.ExtensionContext) {
     console.log('Initializing "code-leaper"!');
 
@@ -12,30 +59,45 @@ export async function activate(context: vscode.ExtensionContext) {
             const editor = vscode.window.activeTextEditor;
             if (!editor) return;
 
-            const helper = new SelectionHelper(editor);
-
-            const basePosition = editor.selection.end;
-
-            // Go to next non-empty line (ignoring comments)
-            const position = helper.isLineEmpty(basePosition)
-                ? helper.nextStart(basePosition)
-                : basePosition;
-
-            let range = helper.getCurrentStatement(position);
-
-            // Clamp to ignore characters such as \r\n
-            // Go to next statement if at the end of statement
-            if (position.isEqual(helper.clampToLine(range.end))) {
-                range = helper.getCurrentStatement(helper.nextStart(position));
-            }
-
-            // Improve selection by getting the largest at the end (should help overall)
-            // ? Maybe make it an option
-            range = helper.getCurrentStatement(range.end);
-
-            moveCursor(editor, range.end);
-            //editor.selection = new vscode.Selection(range.start, range.end);
+            moveCursor(editor, nextStatement(editor).end);
         })
+    );
+
+    context.subscriptions.push(
+        vscode.commands.registerCommand('code-leaper.jumpPrevStatement', () => {
+            const editor = vscode.window.activeTextEditor;
+            if (!editor) return;
+
+            moveCursor(editor, prevStatement(editor).start);
+        })
+    );
+
+    context.subscriptions.push(
+        vscode.commands.registerCommand(
+            'code-leaper.selectNextStatement',
+            () => {
+                const editor = vscode.window.activeTextEditor;
+                if (!editor) return;
+
+                const range = nextStatement(editor);
+                editor.selection = new vscode.Selection(range.start, range.end);
+                editor.revealRange(range);
+            }
+        )
+    );
+
+    context.subscriptions.push(
+        vscode.commands.registerCommand(
+            'code-leaper.selectPrevStatement',
+            () => {
+                const editor = vscode.window.activeTextEditor;
+                if (!editor) return;
+
+                const range = prevStatement(editor);
+                editor.selection = new vscode.Selection(range.end, range.start);
+                editor.revealRange(range);
+            }
+        )
     );
 }
 
